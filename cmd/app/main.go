1afef976148ebfd793f53ebe49076bdf5763b1c8
alexander.karabudka <sanyakarabudka@gmail.com>
2024-05-15 13:34:26 +0200

    add date to show

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,13 +5,14 @@ import (
 	"github.com/labstack/echo/v4"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	fmt.Println("Server running on port 8080")
 
 	e := echo.New()
-	e.GET("/data", Handler)
+	e.GET("/date", Handler)
 
 	err := e.Start(":8080")
 	if err != nil {
@@ -20,7 +21,10 @@ func main() {
 }
 
 func Handler(c echo.Context) error {
-	err := c.String(http.StatusOK, "Hello World")
+	curDate := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 0, 0, 0, 0, time.UTC)
+	durToNewYear := time.Until(time.Date(time.Now().Year()+1, time.January, 1, 0, 0, 0, 0, time.UTC))
+	msg := fmt.Sprintf("Current date: %s. \nDays until New Year: %d", curDate, int64(durToNewYear.Hours())/24)
+	err := c.String(http.StatusOK, msg)
 	if err != nil {
 		return err
 	}
